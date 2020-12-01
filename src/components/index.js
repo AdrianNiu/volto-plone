ec1c12d5c9fc9f27c86e8e1ebec8140a00f92b24
Adrian Niu <niuxx107@umn.edu>
2020-11-30 22:45:50 -0600

    Full view and Album view customization

--- src/components/index.js
+++ src/components/index.js
@@ -11,6 +11,8 @@
 // import AlbumView from './AlbumView/AlbumView';
 import FaqView from './FaqView/FaqView';
 // import FullView from './FullView/FullView';
-import RatingWidget from './RatingWidget/RatingWidget';
+// import RatingWidget from './RatingWidget/RatingWidget';
+import FullView from './FullView/FullView';
+import AlbumView from './AlbumView/AlbumView';
 
-export { FaqView, RatingWidget };
+export { AlbumView, FullView, FaqView };
